fe97abe3dcd0884711d5334bdbbf8b7b17248f32
MateusSantos007 <163445278+MateusSantos007@users.noreply.github.com>
2025-08-08 21:27:21 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,12 +1,13 @@
 <?php
 
 try {
-    $conn = new PDO("sqlsrv:server = tcp:mateussantos.database.windows.net,1433; Database = mateussantos;TrustServerCertificate=false", "mateus", "M1a2t3e4u5s6");
+    $conn = new PDO("sqlsrv:server = tcp:{nome do seu servidor}; Database = {nome do seu banco};TrustServerCertificate=false", "{nome do seu usuario}", "{sua senha}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
 }
+
 
 ?>
